0817222ce386790a42509f3d1254ae8dd0056ed6
ChumaMqeke <94187682+ChumaMqeke@users.noreply.github.com>
2023-11-02 13:41:09 +0200

    Portfolio update

---
+++ src/components/About.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import aboutImg from "../assets/images/cma.png";
+const About = () => {
+  const info = [
+    { text: "Years experience", count: "3" },
+    { text: "Completed Projects", count: "2" },
+    { text: "Companies Work", count: "2" },
+  ];
+  return (
+    <section id="about" className="py-10 text-white">
+      <div className="text-center mt-8">
+        <h3 className="text-4xl font-semibold">
+          About <span className="text-cyan-600">Me</span>
+        </h3>
+        <p className="text-gray-400 my-3 text-lg">My introduction</p>
+        <div className="flex md:flex-row flex-col-reverse items-center md:gap-6 gap-12 px-10 max-w-6xl mx-auto">
+          <div className="p-2">
+            <div className="text-gray-300 my-3">
+              <p className="text-justify leading-7 w-11/12 mx-auto">
+              I'm an IT graduate with an Advanced Diploma in ICT, specializing in application development.
+               I'm actively pursuing a junior Software Developer role to contribute to your team's mission.
+                With one year of experience as a System Developer and six months as a Data Analyst, 
+                I possess a strong foundation in software development and data analysis. 
+                I'm currently enrolled in the UVU Africa 18-month Software Development Program to enhance my technical skills. 
+                I'm dedicated, motivated, and eager to help your organization thrive as a junior Software Developer.
+              </p>
+              <div className="flex mt-10 items-center gap-7">
+                {info.map((content) => (
+                  <div key={content.text}>
+                    <h3 className="md:text-4xl text-2xl font-semibold text-white">
+                      {content.count}
+                      <span className="text-cyan-600">+</span>{" "}
+                    </h3>
+                    <span className="md:text-base text-xs">{content.text}</span>
+                  </div>
+                ))}
+              </div>
+              <br />
+              <br />
+              <a href="./src/assets/Code_a_program.pdf" download>
+                <button className="btn-primary">Download CV</button>
+              </a>
+            </div>
+          </div>
+          <div className="flex-1 md:mt-0 mt-6 flex justify-center items-center">
+            <div className="lg:w-96 h-full relative sm:w-10/12 w-11/12 max-w-sm aboutImg ">
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-full object-cover bg-cyan-600 rounded-xl"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
